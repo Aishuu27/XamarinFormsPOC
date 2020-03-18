69c5caa9e053c6e8d6d90cf168694a5b87e2b9a6
aishwarya <aishwaryamurali11@gmail.com>
2020-03-18 16:32:06 +0530

    Add project files.

---
+++ App1/App1/splashPage.cs
@@ -0,0 +1,37 @@
+﻿using App1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+public class SplashPage:ContentPage
+{	
+	Image splash_logo;
+	public SplashPage()
+	{
+
+		var sub = new AbsoluteLayout();
+		splash_logo = new Image {
+			Source = "splash_logo",
+			HeightRequest = 100,
+			WidthRequest = 100
+		};
+		AbsoluteLayout.SetLayoutFlags(splash_logo, AbsoluteLayoutFlags.PositionProportional);
+		AbsoluteLayout.SetLayoutBounds(splash_logo, new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+		sub.Children.Add(splash_logo);
+		this.BackgroundColor = Color.FromHex("#FFFFFF");
+		this.Content = sub; 
+	}
+	protected async override void OnAppearing() {
+		base.OnAppearing();
+		await splash_logo.ScaleTo(1,5000);
+		Application.Current.MainPage = new NavigationPage(new MainPage());
+
+	}
+
+	
+}
